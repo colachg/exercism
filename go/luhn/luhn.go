6375f5ca76c3b3986c427483f6e639638508f3bb
colachg <colachg@gmail.com>
2020-05-25 11:26:22 +0800

    Finish luhn

---
+++ go/luhn/luhn.go
@@ -0,0 +1,26 @@
+package luhn
+
+// Valid to check string 
+func Valid(s string) bool {
+	var n, d, i, m int
+	for i = len(s) - 1; i >= 0; i-- {
+		c := s[i]
+		switch {
+		case c == ' ':
+			continue
+		case c >= '0' && c <= '9':
+			m = int(c - '0')
+			if d%2 == 1 {
+				m <<= 1
+			}
+			if m > 9 {
+				m -= 9
+			}
+			n += m
+			d++
+		default:
+			return false
+		}
+	}
+	return d > 1 && n%10 == 0
+}
