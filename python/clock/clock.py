54fe001a6756c24890c92cdca8daf6bca95071c3
colachg <colachg@gmail.com>
2020-07-30 14:47:46 +0800

    Refactor to clear

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,30 +1,17 @@
 class Clock:
     def __init__(self, hour, minute):
-        self.hour = (hour + minute // 60) % 24
-        self.minute = minute % 60
+        self.minutes = (minute + 60 * hour) % (24 * 60)
 
     def __repr__(self):
-        return "%02d:%02d" % (self.hour % 24, self.minute % 60)
+        return "%02d:%02d" % (self.minutes // 60, self.minutes % 60)
 
     def __eq__(self, other):
-        if other.hour == self.hour and other.minute == self.minute:
-            return True
-        return False
+        return repr(self) == repr(other)
 
     def __add__(self, minutes):
-        self.hour = self.hour + (self.minute + minutes) // 60
-        self.minute = (self.minute + minutes) % 60
-        return "%02d:%02d" % (self.hour % 24, self.minute % 60)
+        self.minutes = (self.minutes + minutes) % 1440
+        return self
 
     def __sub__(self, minutes):
-        if minutes <= 60:
-            if minutes <= self.minute:
-                self.minute = self.minute - minutes
-            else:
-                self.minute = self.minute - minutes + 60
-                self.hour -= 1
-        else:
-            self.hour += (self.minute - minutes) // 60
-            self.minute = (self.minute - minutes) % 60
-        self.hour += 24 if self.hour < 0 else 0
-        return "%02d:%02d" % (self.hour % 24, self.minute % 60)
+        self.minutes = (self.minutes - minutes) % 1440
+        return self
