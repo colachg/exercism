d4732258d01b56bf29c396ea3632f705c0d0332c
colachg <colachg@gmail.com>
2021-07-02 17:01:12 +0800

    Rebase master

--- python/beiju-text/beiju.py
+++ python/beiju-text/beiju.py
@@ -1,2 +1,33 @@
-def parse(sentence):
-    pass
+from collections import deque
+
+
+def parse(text):
+    append_to_end = True
+    container = deque()
+    current_text = []
+
+    for ch in text:
+        if ch == "[":
+            if append_to_end:
+                container.append("".join(current_text))
+            else:
+                container.appendleft("".join(current_text))
+
+            append_to_end = False
+            current_text = []
+        elif ch == "]":
+            if append_to_end:
+                container.append("".join(current_text))
+            else:
+                container.appendleft("".join(current_text))
+
+            append_to_end = True
+            current_text = []
+        else:
+            current_text.append(ch)
+
+    if append_to_end:
+        container.append("".join(current_text))
+    else:
+        container.appendleft("".join(current_text))
+    return "".join(map(str, container))
--- python/beiju-text/beiju_test.py
+++ python/beiju-text/beiju_test.py
@@ -1,21 +1,29 @@
+"""
+/* UVA11988 Broken Keyboard (a.k.a. Beiju Text)  */
+"""
 import unittest
+from beiju import parse
 
 
-class BeijuTest(unittest):
+class BeijuTest(unittest.TestCase):
     test_cases = [
         {
-            "name": "",
-            "input": "This_is_a_[Beiju]_text",
+            "name": "default",
+            "sentences": "This_is_a_[Beiju]_text",
             "expected": "BeijuThis_is_a__text"
         },
         {
-            "name": "",
-            "input": "[[]][][]Happy_Birthday_to_Tsinghua_University",
+            "name": "test case 1",
+            "sentences": "[[]][][]Happy_Birthday_to_Tsinghua_University",
             "expected": "Happy_Birthday_to_Tsinghua_University"
+        },
+        {
+            "name": "test case 2",
+            "sentences": "abc[def]ed[ed]]d[ddee",
+            "expected": "ddeeeddefabcedd"
         }
     ]
 
     def test_beiju(self):
-        for test_case in self.test_cases:
-            # assert test_case["expected"] == test_case["input"]
-            pass
+        for case in self.test_cases:
+            self.assertEqual(parse(case['sentences']), case['expected'])
